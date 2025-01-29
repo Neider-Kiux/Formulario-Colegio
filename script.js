3fd415764372e8f5b5200e7635ee1870ae768673
Neider Garzón <ngarzon@kiux.com>
2025-01-29 16:09:05 -0500

    [ADD] Agregando condiciones del input y submit

---
+++ script.js
@@ -0,0 +1,15 @@
+document.getElementById('telefono_acudiente').addEventListener('input', (event) => {
+  if (event.target.value.length > 10) {
+    event.target.value = event.target.value.slice(0, 10);
+  }
+});
+
+document.getElementById('Form_1').addEventListener('submit', function (event) {
+  event.preventDefault();
+  const formData = new FormData(this);
+  const jsonData = {};
+  formData.forEach((value, key) => {
+    jsonData[key] = value;
+  });
+  console.log(JSON.stringify(jsonData));
+});
